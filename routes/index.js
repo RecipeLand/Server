eba2fc3957ab945f57307a6918a914d3eb7441f8
shantidyah <shanti.dyah@gmail.com>
2018-08-15 21:05:31 +0700

    edit and delete routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const {create, read, readById, readOne} = require('../controllers/recipeController')
+
+const {create, read, update, deleteRecipe, readById, readOne} = require('../controllers/recipeController')
 
 router.get('/', read);
 router.get('/find/:id', readById);
 router.get('/search', readOne);
 router.post('/create', create);
+router.put('/edit/:id', update)
+router.delete('/delete/:id', deleteRecipe)
 
 module.exports = router;

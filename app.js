bb0d12e5c77803a08d14ebac41766b584c24abf0
Richardson12345 <chuddywarrior@gmail.com>
2018-08-16 14:26:06 +0700

    added tumblr feature

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/recipe', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tumblr', tumblrRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
